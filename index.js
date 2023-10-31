fbc80f3d834c677a24f977436b91d2bc3ea29331
Ankita-079 <146926021+Ankita-079@users.noreply.github.com>
2023-10-31 22:36:13 +0530

    aDD

--- index.js
+++ index.js
@@ -15,14 +15,12 @@ const cors = require("cors");
 
 app.use(express.json());
 
-app.use(
-  cors({
-    // origin: "https://charming-bombolone-60247a.netlify.app",
-    origin:"http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // app.use("/auth", authRouter);
 
